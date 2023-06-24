0d83a03ed6d16e5c8377d0738a2a256325a12c23
RhoumaInes <ines.rhouma@esprit.tn>
2023-06-25 00:44:13 +0100

    first commit

---
+++ src/main/java/tn/esprit/asi/Gestiondesformations1Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.asi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gestiondesformations1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gestiondesformations1Application.class, args);
+	}
+
+}
